8925bef51e765a13cbbb72256850444d7fd0d3f5
mariaeduarda1274 <maria.faustino.78068@a.fecaf.com.br>
2025-11-10 08:23:18 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -80,7 +80,6 @@ print(f"✅ Mapa gerado com sucesso: {out_html}")
 
 
 # CÉLULA 3: BAIXAR O MAPA DE FORMA GARANTIDA!
-cp /content/rota-inteligente--sabor-express/mapa_entregas.html /content/mapa_entregas.html
 from google.colab import files
 files.download("/content/mapa_entregas.html")
 
